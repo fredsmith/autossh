9c84c3de413d2a8178171b066cdb3b5995a5c9a9
Fred Smith <fred.smith@nuance.com>
2013-03-19 15:17:09 -0400

    improve the method for checking if we're already running.  spawn off multiple children with config files

--- launchssh.sh
+++ launchssh.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-SSHPROCS=$(ps ax | grep [a]utossh.sh | wc -l)
-[ $SSHPROCS -eq 0 ] || exit
-
-screen -d -m ~/autossh/autossh.sh
+for CONFIG in ~/autossh/*.tunnel; do
+   SSHPROCS=$(ps uwx | grep "[a]utossh.sh $CONFIG" | wc -l)
+   [ $SSHPROCS -eq 0 ] || exit
+   screen -d -m ~/autossh/autossh.sh $CONFIG
+done
 
